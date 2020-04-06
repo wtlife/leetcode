8f218a3f0f60053abe95d9b07c1a90b4e067bf34
wutao <wutao@wtlifeMac.local>
2020-04-06 17:47:56 +0800

    [feat] average of levels

---
+++ src/code/tree/AverageOfLevels.java
@@ -0,0 +1,35 @@
+package code.tree;
+
+import entity.TreeNode;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Queue;
+
+public class AverageOfLevels {
+    public List<Double> averageOfLevels(TreeNode root) {
+        List<Double> res = new ArrayList<>();
+        if (root == null) {
+            return res;
+        }
+        Queue<TreeNode> queue = new ArrayDeque<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            long size = queue.size();
+            long sum = 0;
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+                sum += node.val;
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+            }
+            res.add(sum*1.0/size);
+        }
+        return res;
+    }
+}
