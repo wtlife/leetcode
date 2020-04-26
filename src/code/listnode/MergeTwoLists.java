3ec41dd0a1ee33a625d28030245b8a910c4db845
wtlife <wtlife@qq.com>
2020-04-26 23:11:15 +0800

    [feat] merge k

---
+++ src/code/listnode/MergeTwoLists.java
@@ -0,0 +1,48 @@
+package code.listnode;
+
+import entity.ListNode;
+
+/**
+ * 21. 合并两个有序链表
+ */
+public class MergeTwoLists {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null) {
+            return l2;
+        }
+        if (l2 == null) {
+            return l1;
+        }
+
+        if (l1.val < l2.val) {
+            l1.next = mergeTwoLists(l1.next, l2);
+            return l1;
+        }
+        l2.next = mergeTwoLists(l1, l2.next);
+        return l2;
+    }
+
+
+    public ListNode mergeTwoLists2(ListNode l1, ListNode l2) {
+        if (l1 == null) {
+            return l2;
+        }
+        if (l2 == null) {
+            return l1;
+        }
+        ListNode first = new ListNode(0), cur = first;
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                cur.next = l1;
+                l1 = l1.next;
+            } else {
+                cur.next = l2;
+                l2 =l2.next;
+            }
+            cur = cur.next;
+        }
+        cur.next = l1==null ? l2:l1;
+
+        return first.next;
+    }
+}
