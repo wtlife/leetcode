a9414b9939a215badd7688550022fb02e2b68c86
wtlife <wtlife@qq.com>
2020-05-07 22:44:15 +0800

    [feat] tree rob3

--- src/code/tree/Rob3.java
+++ src/code/tree/Rob3.java
@@ -2,7 +2,9 @@ package code.tree;
 
 import entity.TreeNode;
 
+import java.util.HashMap;
 import java.util.LinkedList;
+import java.util.Map;
 import java.util.Queue;
 
 /**
@@ -10,7 +12,24 @@ import java.util.Queue;
  */
 public class Rob3 {
 
+    private Map<TreeNode, Integer> memo = new HashMap<>();
+
     public int rob(TreeNode root) {
-        return 0;
+        if (root == null) {
+            return 0;
+        }
+        if (memo.containsKey(root)) {
+            return memo.get(root);
+        }
+        int rob = root.val
+                + (root.left == null ?
+                0 : rob(root.left.left) + rob(root.left.right))
+                + (root.right == null ?
+                0 : rob(root.right.left) + rob(root.right.right));
+        int not_rob = rob(root.left) + rob(root.right);
+
+        int res = Math.max(rob, not_rob);
+        memo.put(root, res);
+        return res;
     }
 }
