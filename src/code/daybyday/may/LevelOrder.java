e9fb4e3fafd22f7a683c0c6954dba0fd3ee6a674
wtlife <wtlife@qq.com>
2020-05-13 19:16:47 +0800

    [may] level

---
+++ src/code/daybyday/may/LevelOrder.java
@@ -0,0 +1,40 @@
+package code.daybyday.may;
+
+import entity.TreeNode;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * 102. 二叉树的层序遍历
+ */
+public class LevelOrder {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> res = new LinkedList<>();
+        if (root == null) {
+            return res;
+        }
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            List<Integer> temp = new LinkedList<>();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+                temp.add(node.val);
+                if (node.left != null) {
+                    queue.add(node.left);
+                }
+                if (node.right != null) {
+                    queue.add(node.right);
+                }
+            }
+            if (temp.size() > 0) {
+                res.add(temp);
+            }
+        }
+        return res;
+    }
+}
