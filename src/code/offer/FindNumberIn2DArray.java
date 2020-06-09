5438961877827e92272dbb61fbe26bdcad7aaea3
wtlife <wtlife@qq.com>
2020-06-09 17:21:12 +0800

    [offer] 04

---
+++ src/code/offer/FindNumberIn2DArray.java
@@ -0,0 +1,30 @@
+package code.offer;
+
+/**
+ * 04. 二维数组中的查找
+ */
+public class FindNumberIn2DArray {
+    public boolean findNumberIn2DArray(int[][] matrix, int target) {
+        if (matrix == null) {
+            return false;
+        }
+        if (matrix.length==0||matrix[0].length==0){
+            return false;
+        }
+        int col = 0;
+        int row = matrix[0].length - 1;
+        while (col < matrix.length && row >= 0) {
+            int temp = matrix[col][row];
+
+            if (temp == target) {
+                return true;
+            } else if (temp > target) {
+                row--;
+            } else {
+                col++;
+            }
+        }
+        return false;
+
+    }
+}
