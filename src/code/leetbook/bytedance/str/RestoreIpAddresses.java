ca906fcb0cf6cfca0034aac5eba7906e5241a597
wtlife <wtlife@qq.com>
2020-09-12 10:22:40 +0800

    [feat]resore ip

--- src/code/leetbook/bytedance/str/RestoreIpAddresses.java
+++ src/code/leetbook/bytedance/str/RestoreIpAddresses.java
@@ -8,7 +8,36 @@ import java.util.List;
  * @date 2020/9/10
  */
 public class RestoreIpAddresses {
+    List<String> list = new ArrayList<>();
+
     public List<String> restoreIpAddresses(String s) {
-        return null;
+        backTrack("", 0, s, 4);
+        return list;
     }
+
+    public void backTrack(String res, int idx, String s, int flag) {
+        if (idx == s.length() && flag==0) {
+            list.add(res.substring(0, res.length() - 1));
+            return;
+        }
+        if (flag < 0) {
+            return;
+        }
+
+        for (int i = idx; i < idx + 3 && i < s.length(); i++) {
+
+            char curChar = s.charAt(i);
+            if (i==idx &&Character.getNumericValue(curChar) == 0) {
+                backTrack(res + s.substring(idx, i + 1) + ".", i + 1, s, flag-1);
+                break;
+            }
+
+            int num = Integer.parseInt(s.substring(idx, i + 1));
+            if (num > 0 && num <= 255) {
+                backTrack(res + num + ".", i + 1, s, flag-1);
+            }
+
+        }
+    }
+
 }
--- src/code/leetbook/bytedance/str/SimplifyPath.java
+++ src/code/leetbook/bytedance/str/SimplifyPath.java
@@ -1,11 +1,37 @@
 package code.leetbook.bytedance.str;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 /**
  * @Desc SimplifyPath
  * @date 2020/9/10
  */
 public class SimplifyPath {
     public String simplifyPath(String path) {
-        return null;
+        Deque<String> stack = new ArrayDeque<>();
+        String[] items = path.split("/");
+        for (String item : items) {
+            if ("".equals(item) || ".".equals(item)) {
+                continue;
+            }
+            if ("..".equals(item)) {
+                if (!stack.isEmpty()) {
+                    stack.pollLast();
+                }
+                continue;
+            }
+            stack.addLast(item);
+        }
+
+        if (stack.isEmpty()) {
+            return "/";
+        }
+
+        String res = "";
+        while (!stack.isEmpty()) {
+            res = "/" + stack.pollLast() + res;
+        }
+        return res;
     }
 }
